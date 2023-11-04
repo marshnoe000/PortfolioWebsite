1dd958c5d1f144b97385e5938ade7191129f602e
Noelle Nielson <noelle.marshall17@gmail.com>
2023-11-04 12:41:14 -0600

    Making sure things work

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>My website!</h1>
         <p>
           Welcome to my portfolio website!
         </p>
